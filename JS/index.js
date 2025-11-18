6f61384c2bbd2d734a79e8e1ee376128349452c0
bmcewan1234 <bmcewan@udesa.edu.ar>
2025-11-18 18:51:32 -0300

    hice la 4 y ademas cambie algo de los html porque los js estaban mal vinculados

--- JS/index.js
+++ JS/index.js
@@ -1,16 +1,52 @@
 let formulario = document.querySelector(".barradebusqueda")
 let inputForm = document.querySelector(".barradebusqueda input")
 
-formulario.addEventListener("submit", function(evento){
+formulario.addEventListener("submit", function (evento) {
     evento.preventDefault()
 
-    if (inputForm.value == ""){
+    if (inputForm.value == "") {
         return alert("por favor esrciba tres caracteres o mas")
     }
-    if (inputForm.value.length < 3){
+    if (inputForm.value.length < 3) {
         return alert("por favor esrciba tres caracteres o mas")
     }
 
     localStorage.setItem("input", inputForm.value)
     this.submit()
 })
+
+let seccionesProductos = document.querySelectorAll(".productos .cajas-productos");
+let seccionCamisetas = seccionesProductos[0];
+let seccionBotines = seccionesProductos[1];
+
+cargarSeccion("camisetas-hombre", seccionCamisetas);
+cargarSeccion("botines-hombre", seccionBotines);
+
+function cargarSeccion(nombreCategoria, contenedor){
+    fetch("https://dummyjson.com/products/category/" + nombreCategoria)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let productos = data.products;
+            let contenido = "";
+
+            for (let i = 0; i < 10; i++) {
+                let p = productos[i];
+                contenido += `
+                    <article>
+                        <img src="${p.thumbnail}" alt="${p.title}">
+                        <h3>${p.title}</h3>
+                        <p>${p.description}</p>
+                        <h4>$${p.price}</h4>
+                        <a href="product.html?id=${p.id}">Ver detalle</a>
+                    </article>
+                `;
+            }
+            contenedor.innerHTML = contenido;
+        })
+        .catch(function (error) {
+            console.log("El error es: " + error);
+            contenedor.innerHTML = "<p>Hubo un error al cargar los productos</p>";
+        })    
+}
